bde960e94f9124c71dcbeab8fda86d72509c6347
Metin ATALAY <metin.atalay@architecht.com>
2022-01-01 18:52:32 +0300

    Initial Commit

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Metin Atalay on 1.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
